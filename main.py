d24d141c33e042e49de88d9ecb26def0219f3649
Esdras Battosti <esdrasbattostisilva@gmail.com>
2022-12-24 17:49:55 -0300

    refactor: removing useless attributes
    - Prisoner: success attribute
    - Drawer: opened attribute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 class Prisoner:
     def __init__(self, number):
         self.number = number
-        self.success = False
         self.choices = 50
 
     def __repr__(self):
@@ -18,7 +17,6 @@ class Drawer:
     def __init__(self, number, card):
         self.number = number
         self.card = card
-        self.opened = False
 
     def __repr__(self):
         return f"Drawer({self.number}, {self.card})"
